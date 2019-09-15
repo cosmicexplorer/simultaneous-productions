7cedd03696f85e04d840ff6c21acf2092803860d
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2019-09-14 21:00:46 -0700

    finally make things compile again!!

--- src/lib.rs
+++ src/lib.rs
@@ -52,68 +52,332 @@
 // Arc<Mutex> can be more clear than needing to grok Orderings:
 #![allow(clippy::mutex_atomic)]
 
+/* #[macro_use] */
 extern crate frunk;
 /* #[macro_use] */
 /* extern crate gensym; */
 extern crate indexmap;
 extern crate priority_queue;
+/* #[macro_use] */
+/* extern crate quote; */
 extern crate typename;
 
+/* use gensym::gensym; */
 use indexmap::{IndexMap, IndexSet};
 use priority_queue::PriorityQueue;
 use typename::TypeName;
 
+/* use frunk::hlist::*; */
 use std::{
   collections::{HashMap, HashSet, VecDeque},
-  fmt::Debug,
+  fmt::{self, Debug},
   hash::{Hash, Hasher},
+  rc::Rc,
 };
 
 
 pub mod binding {
-  /* use super::{user_api::*, *}; */
+  use super::{grammar_indexing::*, lowering_to_indices::*, user_api::*, *};
 
-  /* #[derive(Debug, Clone, PartialEq, Eq, Hash)] */
-  /* pub struct BindingError(String); */
+  #[derive(Debug, Clone, PartialEq, Eq, Hash)]
+  pub struct BindingError(String);
 
-  /* pub trait Acceptor<Input, Output> { */
-  /* fn accept(&self, input: Input) -> Result<Output, BindingError>; */
+  pub trait ProvidesProduction<Tok: Debug+PartialEq+Eq+Hash+Copy+Clone+TypeName> {
+    fn as_production(&self) -> Production<Tok>;
+    fn get_type_name(&self) -> TypeNameWrapper;
+    fn get_acceptors(&self) -> Vec<Rc<Box<dyn PointerBoxingAcceptor>>>;
+  }
+
+  #[derive(Debug, Clone, PartialEq, Eq, Hash, TypeName)]
+  pub struct TypeNameWrapper(String);
+
+  impl TypeNameWrapper {
+    pub fn for_type<T: TypeName>() -> Self { TypeNameWrapper(T::type_name()) }
+
+    pub fn as_production_reference(&self) -> ProductionReference {
+      ProductionReference::new(&self.0)
+    }
+  }
+
+  #[derive(Debug, Clone, PartialEq, Eq, TypeName)]
+  pub struct TypedCase<Tok: Debug+PartialEq+Eq+Hash+Copy+Clone+TypeName> {
+    pub case: Case<Tok>,
+    pub acceptor: Rc<Box<dyn PointerBoxingAcceptor>>,
+  }
+
+  #[derive(Debug, Clone, PartialEq, Eq, TypeName)]
+  pub struct TypedProduction<Tok: Debug+PartialEq+Eq+Hash+Copy+Clone+TypeName> {
+    pub cases: Vec<TypedCase<Tok>>,
+    pub output_type: TypeNameWrapper,
+  }
+
+  impl<Tok: Debug+PartialEq+Eq+Hash+Copy+Clone+TypeName> ProvidesProduction<Tok>
+    for TypedProduction<Tok>
+  {
+    fn as_production(&self) -> Production<Tok> {
+      Production(
+        self
+          .cases
+          .iter()
+          .map(|TypedCase { case, .. }| case)
+          .cloned()
+          .collect(),
+      )
+    }
+
+    fn get_type_name(&self) -> TypeNameWrapper { self.output_type.clone() }
+
+    fn get_acceptors(&self) -> Vec<Rc<Box<dyn PointerBoxingAcceptor>>> {
+      self
+        .cases
+        .iter()
+        .cloned()
+        .map(|TypedCase { acceptor, .. }| acceptor)
+        .collect()
+    }
+  }
+
+  #[derive(Debug, PartialEq, Eq, TypeName)]
+  pub struct TypedSimultaneousProductions<
+    Tok: Debug+PartialEq+Eq+Hash+Copy+Clone+TypeName,
+    /* Members: HList, */
+  > {
+    underlying: SimultaneousProductions<Tok>,
+    bindings: IndexMap<ProdCaseRef, Rc<Box<dyn PointerBoxingAcceptor>>>,
+  }
+
+  impl<
+      Tok: Debug+PartialEq+Eq+Hash+Copy+Clone+TypeName,
+      /* Members: HList, */
+    >
+    TypedSimultaneousProductions<
+      Tok,
+      /* Members, */
+    >
+  {
+    pub fn new(production_boxes: Vec<Rc<Box<dyn ProvidesProduction<Tok>>>>) -> Self {
+      let underlying = SimultaneousProductions(
+        production_boxes
+          .iter()
+          .cloned()
+          .map(|prod| {
+            (
+              prod.get_type_name().as_production_reference(),
+              prod.as_production(),
+            )
+          })
+          .collect(),
+      );
+      let bindings: IndexMap<ProdCaseRef, Rc<Box<dyn PointerBoxingAcceptor>>> =
+        production_boxes
+          .iter()
+          .cloned()
+          .enumerate()
+          .flat_map(|(prod_ind, prod)| {
+            let cur_prod_ref = ProdRef(prod_ind);
+            prod
+              .get_acceptors()
+              .into_iter()
+              .enumerate()
+              .map(move |(case_ind, acceptor)| {
+                let cur_prod_case_ref = ProdCaseRef {
+                  prod: cur_prod_ref,
+                  case: CaseRef(case_ind),
+                };
+                (cur_prod_case_ref, acceptor)
+              })
+              .collect::<Vec<_>>()
+          })
+          .collect();
+      TypedSimultaneousProductions {
+        underlying,
+        bindings,
+      }
+    }
+  }
+
+  #[derive(Debug, Clone, PartialEq, Eq, Hash, TypeName)]
+  pub struct ParamName(String);
+
+  impl ParamName {
+    pub fn new(s: &str) -> Self { ParamName(s.to_string()) }
+  }
+
+  #[derive(Debug, Clone, PartialEq, Eq, Hash, TypeName)]
+  pub struct TypedParam {
+    arg_type: TypeNameWrapper,
+    arg_name: ParamName,
+  }
+
+  impl TypedParam {
+    pub fn new<T: TypeName>(arg_name: ParamName) -> Self {
+      TypedParam {
+        arg_type: TypeNameWrapper::for_type::<T>(),
+        arg_name,
+      }
+    }
+  }
+
+  #[derive(Debug, Clone, PartialEq, Eq, Hash, TypeName)]
+  pub struct TypedProductionParamsDescription {
+    output_type: TypeNameWrapper,
+    params: Vec<TypedParam>,
+  }
+
+  impl TypedProductionParamsDescription {
+    pub fn new<T: TypeName>(params: Vec<TypedParam>) -> Self {
+      TypedProductionParamsDescription {
+        output_type: TypeNameWrapper::for_type::<T>(),
+        params,
+      }
+    }
+  }
+
+  pub trait PointerBoxingAcceptor {
+    fn identity_salt(&self) -> &str;
+    fn type_params(&self) -> TypedProductionParamsDescription;
+    fn accept_erased(
+      &self,
+      args: Vec<Rc<Box<dyn std::any::Any>>>,
+    ) -> Rc<Box<dyn std::any::Any>>;
+  }
+
+  impl Debug for dyn PointerBoxingAcceptor {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+      write!(f, "PointerBoxingAcceptor({:?})", self.type_params())
+    }
+  }
+  impl PartialEq for dyn PointerBoxingAcceptor {
+    fn eq(&self, other: &Self) -> bool { self.identity_salt() == other.identity_salt() }
+  }
+  impl Eq for dyn PointerBoxingAcceptor {}
+  impl Hash for dyn PointerBoxingAcceptor {
+    fn hash<H: Hasher>(&self, state: &mut H) { self.identity_salt().hash(state); }
+  }
+  impl TypeName for dyn PointerBoxingAcceptor {
+    fn fmt(f: &mut fmt::Formatter<'_>) -> fmt::Result {
+      write!(f, "dyn {}::PointerBoxingAcceptor", module_path!())
+    }
+  }
+
+  /* #[macro_export] */
+  /* macro_rules! _merge { */
+  /* /\* This allows merging the head/tail! *\/ */
+  /* (@merge [], [$($rest:tt)*]) => { [$($rest)*] }; */
+  /* (@merge [$($cur:tt)*], []) => { [$($cur)*] }; */
+  /* (@merge */
+  /* [$($cur_arg_name:ident: $cur_arg_type:ty),+], */
+  /* [$($rest_arg_name:ident: $rest_arg_type:ty),+]) => { */
+  /* [ */
+  /* $($cur_arg_name: $cur_arg_type),+ */
+  /* , */
+  /* $($rest_arg_name: $rest_arg_type),+ */
+  /* ] */
+  /* }; */
+  /* ([$(rest:tt)+]) => { [$($rest)+] } */
+  /* } */
+
+  /* #[macro_export] */
+  /* macro_rules! _extract_typed_params { */
+  /* ([$arg_name:ident: $arg_type:ty]) => { */
+  /* [$arg_name: $arg_type] */
+  /* }; */
+  /* ([$_literal:expr]) => { [] }; */
+
+  /* ($([$arg_name:ident: $arg_type:ty]),+) => { */
+  /* [ */
+  /* $( */
+  /* $arg_name: $arg_type */
+  /* ),+ */
+  /* ] */
+  /* }; */
+
+  /* ([$arg_name:ident: $arg_type:ty], [$_literal:expr], $($rest:tt)+) => { */
+  /* _extract_typed_params![[$arg_name: $arg_type], $($rest:tt)+] */
+  /* }; */
+  /* ([$arg_name:ident: $arg_type:ty], [$_literal:expr]) => { */
+  /* [$arg_name: $arg_type] */
+  /* }; */
+
+  /* ([$_literal:expr], $($rest:tt)+) => { _extract_typed_params![$($rest)+]
+   * }; */
+  /* } */
+
+  /* fn wow() { */
+  /* trace_macros!(true); */
+  /* _extract_typed_params![["a"]]; */
+  /* _extract_typed_params![[y: u32]]; */
+  /* _extract_typed_params![["a"], [y: u32]]; */
+  /* _extract_typed_params![[y: u32], ["XXX"]]; */
+  /* trace_macros!(false); */
+  /* } */
+
+  /* #[macro_export] */
+  /* macro_rules! _generate_typed_params_description { */
+  /* ($production_type:ty, [$($arg_name:ident: $arg_type:ty),+]) => { */
+  /* TypedProductionParamsDescription::new::<$production_type>(vec![ */
+  /* $( */
+  /* TypedParam::new::<$arg_type>(ParamName::new(stringify!($a))) */
+  /* ),+ */
+  /* ]) */
+  /* }; */
+  /* } */
+
+  /* #[macro_export] */
+  /* macro_rules! _generate_case { */
+  /* ($gen_id:ident, $production_type:ty => [$($decl:tt)+] => $body:block) =>
+   * {{ */
+  /* pub struct $gen_id(pub String); */
+  /* impl PointerBoxingAcceptor> for $gen_id { */
+  /* fn identity_salt(&self) -> &str { */
+  /* self.0.as_str() */
+  /* } */
+
+  /* fn type_params(&self) -> TypedProductionParamsDescription { */
+  /* _generate_typed_params_description![$production_type,
+   * _extract_typed_params![$($decl)+]] */
+  /* } */
+
+  /* fn accept(args: Vec<Box<dyn std::any::Any>>) -> $production_type { */
+  /* let rev_args: Vec<_> = args.into_iter().rev().collect(); */
+  /* $( */
+  /* let $a: $in = rev_args.pop() */
+  /* .expect("failed to pop from argument vector!") */
+  /* .downcast::<$in>() */
+  /* .expect("invalid downcast!"); */
+  /* )* */
+  /* $body */
+  /* } */
+  /* } */
+  /* let acceptor = Rc::<dyn
+   * PointerBoxingAcceptor>>::new( */
+  /* $gen_id(format!("anonymous class at {}::{}", module_path!(),
+   * stringify!($gen_id)))); */
+  /* let case = Case(vec![$($x),*]); */
+  /* TypedCase { case, acceptor } */
+  /* }}; */
+  /* } */
+
+  /* #[macro_export] */
+  /* macro_rules! productions { */
+  /* ($($production_type:ty => [ */
+  /* $(case ($($decl:tt)+) => $body:block),+ */
+  /* ]),+) => { */
+  /* TypedSimultaneousProductions::new(vec![ */
+  /* $((Box::new(TypedProduction(vec![ */
+  /* $( */
+  /* gensym!{ _generate_case!{ $production_type => [$($decl)+] => $body } } */
+  /* ),* */
+  /* ])) */
+  /* )),* */
+  /* ]) */
+  /* }; */
   /* } */
 }
 
 
 pub mod user_api {
-  /* use super::{binding::*, *}; */
   use super::*;
-
-  /* #[macro_export] */
-  /* macro_rules! productions { */
-  /* ($($production_type:ty => [ */
-  /* $(case ($($a:ident: $t:ty => $x:expr),*) => $case_function_body:block),* */
-  /* ]),*) => {{ */
-  /* SimultaneousProductions([ */
-  /* $((ProductionReference::<$production_type>::new(), */
-  /* Production(vec![ */
-  /* $({ */
-  /* pub struct Args { */
-  /* $(pub $a: $t),* */
-  /* } */
-
-  /* impl Producer<$production_type> for Args { */
-  /* fn produce(&self) -> ProducerResult<$production_type> { */
-  /* match self.clone() { */
-  /* Args { $($a),* } => $case_function_body */
-  /* } */
-  /* } */
-  /* } */
-
-  /* Case::<_, ($($t),*)>(($($x),*): ($($t),*)) */
-  /* }),* */
-  /* ]) */
-  /* )),* */
-  /* ].iter().cloned().collect()) */
-  /* }}; */
-  /* } */
 
   #[derive(Debug, Clone, PartialEq, Eq, Hash, TypeName)]
   pub struct Literal<Tok: Debug+PartialEq+Eq+Hash+Copy+Clone+TypeName>(pub Vec<Tok>);
@@ -4056,26 +4320,28 @@ mod tests {
   /* #[test] */
   /* fn idk() { */
   /* assert_eq!( */
-  /* Something("".to_string()).join(hlist![4.0, 3, "a", Literal::from("a")]), */
-  /* CaseResult("a".to_string()) */
-  /* ); */
-
-  /* use super::user_api::*; */
-  /* assert_eq!( */
   /* { */
   /* trace_macros!(true); */
   /* let res = productions![ */
+  /* u32 => [ */
+  /* case ( */
+  /* _x: Vec<char> => CaseElement::Lit(Literal::from("1")) */
+  /* ) => { */
+  /* 1 */
+  /* } */
+  /* ], */
   /* Vec<i64> => [ */
   /* case ( */
-  /* x: Vec<char> => CaseElement::Lit(Literal::from("a")), */
+  /* _x: Vec<char> => CaseElement::Lit(Literal::from("a")), */
   /* y: u32 => CaseElement::Prod(ProductionReference::<u32>::new()), */
-  /* z: Vec<char> => CaseElement::Lit(Literal::from("a")) */
+  /* _z: Vec<char> => CaseElement::Lit(Literal::from("a")) */
   /* ) => { */
   /* asdf(); */
   /* } */
   /* ] */
   /* ]; */
   /* trace_macros!(false); */
+  /* let res2 = ; */
   /* res */
   /* }, */
   /* SimultaneousProductions( */
