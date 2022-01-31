aa19498538481638476e94a6dcb7df84ef60681f
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2022-01-31 13:21:01 -0500

    add column width to .dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,13 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((rust-mode . ((rust-indent-offset . 2)
-               (comment-start . "/* ") (comment-end . " */")))
- (rustic-mode . ((rustic-indent-offset . 2)
-                 (comment-start . "/* ") (comment-end . " */"))))
+((rust-mode . ((highlight-80+-columns . 100)
+               (fill-column . 100)
+               (rust-indent-offset . 2)
+               (comment-start . "/* ")
+               (comment-end . " */")))
+ (rustic-mode . ((highlight-80+-columns . 100)
+                 (fill-column . 100)
+                 (rustic-indent-offset . 2)
+                 (comment-start . "/* ")
+                 (comment-end . " */"))))
