6360b5d73b5b9decd198ff51f907d1e11d05fced
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2023-03-22 08:53:41 -0400

    finally finish the text grammar parsing arc!!!!

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -589,8 +589,12 @@ pub mod text_backend {
       pub struct $type_name(<Vec<$item> as IntoIterator>::IntoIter);
 
       impl $type_name {
-        fn as_new_vec(&self) -> Vec<$item> {
+        pub fn as_new_vec(&self) -> Vec<$item> {
           self.0.clone().collect()
+        }
+
+        pub fn via_into_iter(x: <Vec<$item> as IntoIterator>::IntoIter) -> Self {
+          Self(x)
         }
       }
 
@@ -639,6 +643,18 @@ pub mod text_backend {
         fn from(value: &str) -> Self {
           let chars = value.chars().collect::<Vec<_>>();
           Self::from(&chars[..])
+        }
+      }
+
+      impl ::core::cmp::PartialOrd for $type_name {
+        fn partial_cmp(&self, other: &Self) -> Option<::core::cmp::Ordering> {
+          self.as_new_vec().partial_cmp(&other.as_new_vec())
+        }
+      }
+
+      impl ::core::cmp::Ord for $type_name {
+        fn cmp(&self, other: &Self) -> ::core::cmp::Ordering {
+          self.as_new_vec().cmp(&other.as_new_vec())
         }
       }
 
@@ -1280,5 +1296,213 @@ $P_2$: $P_1$ -> <bc>
     let graphvizier::generator::DotOutput(output) = gb.build(gv::Id::new("test_sp_graph"));
 
     assert_eq!(output, "digraph test_sp_graph {\n  compound = true;\n\n  subgraph prods {\n    label = \"Productions\";\n    cluster = true;\n    rank = same;\n\n    color = \"blue\";\n    fontcolor = \"blue\";\n    node [color=\"blue\", fontcolor=\"blue\", ];\n\n    prod_P_1[label=\"#P_1\", ];\n    prod_P_2[label=\"#P_2\", ];\n  }\n\n  subgraph P_1_prod {\n    label = \"Cases: \\#P_1\";\n    cluster = true;\n    rank = same;\n\n    color = \"purple\";\n    fontcolor = \"purple\";\n\n    subgraph P_1_case_0 {\n      label = \"0\";\n      cluster = true;\n      rank = same;\n\n      color = \"green4\";\n      fontcolor = \"green4\";\n\n      vertex_0[label=\"<abc>\", color=\"brown\", fontcolor=\"brown\", ];\n    }\n    subgraph P_1_case_1 {\n      label = \"1\";\n      cluster = true;\n      rank = same;\n\n      color = \"green4\";\n      fontcolor = \"green4\";\n\n      vertex_1[label=\"<a>\", color=\"brown\", fontcolor=\"brown\", ];\n      vertex_2[label=\"ref: P_1\", color=\"darkgoldenrod\", fontcolor=\"darkgoldenrod\", ];\n      vertex_3[label=\"<c>\", color=\"brown\", fontcolor=\"brown\", ];\n    }\n    subgraph P_1_case_2 {\n      label = \"2\";\n      cluster = true;\n      rank = same;\n\n      color = \"green4\";\n      fontcolor = \"green4\";\n\n      vertex_4[label=\"<bc>\", color=\"brown\", fontcolor=\"brown\", ];\n      vertex_5[label=\"ref: P_2\", color=\"darkgoldenrod\", fontcolor=\"darkgoldenrod\", ];\n    }\n  }\n\n  prod_P_1 -> vertex_0[color=\"red\", ];\n\n  vertex_0 -> prod_P_1[color=\"black\", ];\n\n  prod_P_1 -> vertex_1[color=\"red\", ];\n\n  vertex_2 -> prod_P_1[color=\"darkgoldenrod\", ];\n\n  vertex_1 -> vertex_2[color=\"aqua\", ];\n\n  vertex_2 -> vertex_3[color=\"aqua\", ];\n\n  vertex_3 -> prod_P_1[color=\"black\", ];\n\n  prod_P_1 -> vertex_4[color=\"red\", ];\n\n  vertex_5 -> prod_P_2[color=\"darkgoldenrod\", ];\n\n  vertex_4 -> vertex_5[color=\"aqua\", ];\n\n  vertex_5 -> prod_P_1[color=\"black\", ];\n\n  subgraph P_2_prod {\n    label = \"Cases: \\#P_2\";\n    cluster = true;\n    rank = same;\n\n    color = \"purple\";\n    fontcolor = \"purple\";\n\n    subgraph P_2_case_0 {\n      label = \"0\";\n      cluster = true;\n      rank = same;\n\n      color = \"green4\";\n      fontcolor = \"green4\";\n\n      vertex_6[label=\"ref: P_1\", color=\"darkgoldenrod\", fontcolor=\"darkgoldenrod\", ];\n    }\n    subgraph P_2_case_1 {\n      label = \"1\";\n      cluster = true;\n      rank = same;\n\n      color = \"green4\";\n      fontcolor = \"green4\";\n\n      vertex_7[label=\"ref: P_2\", color=\"darkgoldenrod\", fontcolor=\"darkgoldenrod\", ];\n    }\n    subgraph P_2_case_2 {\n      label = \"2\";\n      cluster = true;\n      rank = same;\n\n      color = \"green4\";\n      fontcolor = \"green4\";\n\n      vertex_8[label=\"ref: P_1\", color=\"darkgoldenrod\", fontcolor=\"darkgoldenrod\", ];\n      vertex_9[label=\"<bc>\", color=\"brown\", fontcolor=\"brown\", ];\n    }\n  }\n\n  vertex_6 -> prod_P_1[color=\"darkgoldenrod\", ];\n\n  prod_P_2 -> vertex_6[color=\"red\", ];\n\n  vertex_6 -> prod_P_2[color=\"black\", ];\n\n  vertex_7 -> prod_P_2[color=\"darkgoldenrod\", ];\n\n  prod_P_2 -> vertex_7[color=\"red\", ];\n\n  vertex_7 -> prod_P_2[color=\"black\", ];\n\n  vertex_8 -> prod_P_1[color=\"darkgoldenrod\", ];\n\n  prod_P_2 -> vertex_8[color=\"red\", ];\n\n  vertex_8 -> vertex_9[color=\"aqua\", ];\n\n  vertex_9 -> prod_P_2[color=\"black\", ];\n}\n");
+  }
+
+  #[cfg(test)]
+  pub mod proptest_strategies {
+    use super::*;
+
+    use proptest::{prelude::*, strategy::Strategy};
+
+    prop_compose! {
+      pub fn production_name(ensure_cash: bool)
+        (prod_name in prop::string::string_regex(".*").unwrap(),
+         selector in any::<prop::sample::Selector>()) -> ProductionReference {
+          if ensure_cash {
+            let cash_index = selector.select(
+              prod_name.as_str().char_indices().map(|(i, _)| i)
+                .chain([prod_name.len()].iter().cloned())
+            );
+            let (left, right) = prod_name.as_str().split_at(cash_index);
+            let new_prod_name = format!("{0}${1}", left, right);
+            ProductionReference::from(new_prod_name.as_str())
+          } else {
+            ProductionReference::from(prod_name.as_str())
+          }
+        }
+    }
+    prop_compose! {
+      pub fn prod_names(ensure_cash: bool, min_size: usize, max_size: usize)
+        (all_names in prop::collection::vec(production_name(ensure_cash), min_size..=max_size)
+         .prop_filter("no duplicate prod names allowed",
+                      |v| {
+                        let mut v_unique = v.clone();
+                        v_unique.sort_unstable();
+                        v_unique.dedup();
+                        v_unique.len() == v.len()
+                      })
+        ) -> Vec<ProductionReference> {
+        all_names
+      }
+    }
+    prop_compose! {
+      pub fn literal(ensure_arrow: bool)
+        (lit in prop::string::string_regex(".*").unwrap(),
+         selector in any::<prop::sample::Selector>()) -> Lit {
+          if ensure_arrow {
+            let arrow_index = selector.select(
+              lit.as_str().char_indices().map(|(i, _)| i)
+                .chain([lit.len()].iter().cloned())
+            );
+            let (left, right) = lit.as_str().split_at(arrow_index);
+            let new_lit = format!("{0}>{1}", left, right);
+            Lit::from(new_lit.as_str())
+          } else {
+            Lit::from(lit.as_str())
+          }
+        }
+    }
+    prop_compose! {
+      pub fn valid_prod_ref(refs: Vec<ProductionReference>)
+        (ref_index in 0..refs.len()) -> ProductionReference {
+          refs[ref_index].clone()
+        }
+    }
+    pub fn case_element(
+      refs: Vec<ProductionReference>,
+      ensure_arrow: bool,
+    ) -> impl Strategy<Value = CE> {
+      prop_oneof![
+        literal(ensure_arrow).prop_map(|lit| CE::Lit(lit)),
+        valid_prod_ref(refs).prop_map(|prod_ref| CE::Prod(prod_ref)),
+      ]
+    }
+    pub fn case(
+      refs: Vec<ProductionReference>,
+      ensure_arrow: bool,
+      min_length: usize,
+      max_length: usize,
+    ) -> impl Strategy<Value = Case> {
+      prop::collection::vec(case_element(refs, ensure_arrow), min_length..=max_length)
+        .prop_map(|elements| Case::via_into_iter(elements.into_iter()))
+    }
+    pub fn production(
+      refs: Vec<ProductionReference>,
+      ensure_arrow: bool,
+      min_case_length: usize,
+      max_case_length: usize,
+      min_size: usize,
+      max_size: usize,
+    ) -> impl Strategy<Value = Production> {
+      prop::collection::vec(
+        case(refs, ensure_arrow, min_case_length, max_case_length),
+        min_size..=max_size,
+      )
+      .prop_map(|cases| Production::via_into_iter(cases.into_iter()))
+    }
+    pub fn sp(
+      ensure_cash: bool,
+      ensure_arrow: bool,
+      min_prods: usize,
+      max_prods: usize,
+      min_cases: usize,
+      max_cases: usize,
+      min_case_els: usize,
+      max_case_els: usize,
+    ) -> impl Strategy<Value = SP> {
+      prod_names(ensure_cash, min_prods, max_prods)
+        .prop_flat_map(move |names: Vec<ProductionReference>| {
+          let n = names.len();
+          (
+            Just(names.clone()),
+            prop::collection::vec(
+              production(
+                names,
+                ensure_arrow.clone(),
+                min_case_els,
+                max_case_els,
+                min_cases,
+                max_cases,
+              ),
+              n,
+            ),
+          )
+        })
+        .prop_map(
+          |(names, prods): (Vec<ProductionReference>, Vec<Production>)| {
+            let joined: Vec<(ProductionReference, Production)> =
+              names.into_iter().zip(prods.into_iter()).collect();
+            SP::via_into_iter(joined.into_iter())
+          },
+        )
+        .prop_filter("must have at least one prod ref", move |sp: &SP| {
+          !ensure_cash || {
+            sp.as_new_vec().into_iter().any(|(_, prod)| {
+              prod.as_new_vec().into_iter().any(|case| {
+                case
+                  .as_new_vec()
+                  .into_iter()
+                  .any(|case_el| matches![case_el, CE::Prod(_)])
+              })
+            })
+          }
+        })
+        .prop_filter("must have at least one nonempty literal", move |sp: &SP| {
+          !ensure_arrow || {
+            sp.as_new_vec().into_iter().any(|(_, prod)| {
+              prod.as_new_vec().into_iter().any(|case| {
+                case.as_new_vec().into_iter().any(|case_el| match case_el {
+                  CE::Lit(lit) => {
+                    assert!(!lit.into_string().is_empty());
+                    true
+                  },
+                  _ => false,
+                })
+              })
+            })
+          }
+        })
+    }
+
+    #[test]
+    fn test_prod_vec() {
+      use proptest::{strategy::ValueTree, test_runner::TestRunner};
+
+      let mut runner = TestRunner::deterministic();
+      let prod_names: Vec<ProductionReference> = prod_names(false, 2, 5)
+        .new_tree(&mut runner)
+        .unwrap()
+        .current();
+      assert!(prod_names.len() >= 2);
+      assert!(prod_names.len() <= 5);
+      assert_ne!(prod_names[0], prod_names[1]);
+    }
+  }
+
+  #[cfg(test)]
+  mod test {
+    use super::{proptest_strategies::*, *};
+    use gs::constraints::SerializableGrammar;
+
+    use proptest::prelude::*;
+
+    proptest! {
+      #[test]
+      fn test_serde(sp in sp(false, false, 1, 20, 1, 5, 1, 5)) {
+        let text_grammar = sp.serialize().unwrap();
+        assert_eq!(sp, SP::parse(&text_grammar).unwrap());
+      }
+    }
+    proptest! {
+      #[test]
+      fn test_serde_cash(sp in sp(true, false, 1, 20, 1, 5, 1, 5)) {
+        let text_grammar = sp.serialize().unwrap();
+        assert_eq!(sp, SP::parse(&text_grammar).unwrap());
+      }
+    }
+    proptest! {
+      #[test]
+      fn test_serde_arrow(sp in sp(false, true, 1, 20, 1, 5, 1, 5)) {
+        let text_grammar = sp.serialize().unwrap();
+        assert_eq!(sp, SP::parse(&text_grammar).unwrap());
+      }
+    }
+    proptest! {
+      #[test]
+      fn test_serde_cash_arrwo(sp in sp(true, true, 1, 20, 1, 5, 1, 5)) {
+        let text_grammar = sp.serialize().unwrap();
+        assert_eq!(sp, SP::parse(&text_grammar).unwrap());
+      }
+    }
   }
 }
